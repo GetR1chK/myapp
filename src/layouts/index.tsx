937fc5dd77d74cf4ccd5eab754ed7270a314e343
GetR1chK <kongpei0502@163.com>
2023-03-15 17:14:37 +0800

    feat: layout部分搭建

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,21 +1,48 @@
+import { Layout, Menu } from 'antd';
 import { Link, Outlet } from 'umi';
-import styles from './index.less';
+import type { MenuProps } from 'antd';
 
-export default function Layout() {
+const { Sider, Header, Content, Footer } = Layout;
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  childern?: MenuItem[],
+): MenuItem {
+  return {
+    key,
+    icon,
+    childern,
+    label,
+  } as MenuItem;
+}
+
+const items: MenuItem[] = [
+  getItem('Home', '1', <Link to="/"></Link>),
+  getItem('Docs', '2', <Link to="/docs"></Link>),
+];
+
+export default function BasicLayout() {
   return (
-    <div className={styles.navs}>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/docs">Docs</Link>
-        </li>
-        <li>
-          <a href="https://github.com/umijs/umi">Github</a>
-        </li>
-      </ul>
-      <Outlet />
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider>
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={['1']}
+          mode="inline"
+          items={items}
+        ></Menu>
+      </Sider>
+      <Layout>
+        <Header></Header>
+        <Content>
+          <Outlet />
+        </Content>
+        <Footer></Footer>
+      </Layout>
+    </Layout>
   );
 }
